29c2e7236235b5274e6877242a133a1eaeeb21b3
mityaiii <pericle04@mail.ru>
2024-06-12 00:10:00 +0300

    hello world

---
+++ src/models/place/time-picker.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isActive = ref(false)
+
+const toggleButton = () => {
+  isActive.value = !isActive.value
+}
+</script>
+
+<template>
+  <button 
+    class="time-picker px-6 py-4 border-2 border-black rounded-lg transition-colors"
+    :class="{ 'bg-primary text-white': isActive }" 
+    @click="toggleButton"
+  >
+    <p>14:00</p>
+  </button>
+</template>
+
+<style scoped>
+.time-picker {
+}
+</style>
---
+++ src/pages/place.page.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+import { type DateValue, getLocalTimeZone, today } from '@internationalized/date'
+import { Calendar } from '@/components/ui/calendar'
+import TimePicker from '@/models/place/time-picker.vue'
+
+const value = ref(today(getLocalTimeZone())) as Ref<DateValue>
+</script>
+
+<template>
+  <div class="">
+    <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border" />
+    <TimePicker></TimePicker>
+  </div>
+</template>

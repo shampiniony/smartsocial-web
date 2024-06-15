6235c4764fa3c15c4bf833e3a24b0056ebdef51a
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-06-15 06:52:00 +0300

    changed ical lib

--- src/pages/temporary-calendar.page.vue
+++ src/pages/temporary-calendar.page.vue
@@ -14,6 +14,25 @@ import {
 } from '@schedule-x/calendar'
 import { createEventRecurrencePlugin } from "@schedule-x/event-recurrence"
 import '@schedule-x/theme-default/dist/index.css'
+import ical from 'node-ical'
+
+const directEvents = ical.sync.parseICS(`
+BEGIN:VCALENDAR
+VERSION:2.0
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+SUMMARY:Hey look! An example event!
+DTSTART;TZID=America/New_York:20130802T103400
+DTEND;TZID=America/New_York:20130802T110400
+LOCATION:1000 Broadway Ave.\, Brooklyn
+DESCRIPTION: Do something in NY.
+STATUS:CONFIRMED
+UID:7014-1567468800-1567555199@peterbraden@peterbraden.co.uk
+END:VEVENT
+END:VCALENDAR
+`);
+// log the ids of these events
+console.log(Object.keys(directEvents));
 
 const calendarApp = createCalendar({
   selectedDate: '2024-06-15',

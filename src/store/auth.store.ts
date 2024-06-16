f4052124036c31e5c9b157c08ccfb0682d624ac7
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-06-16 10:41:00 +0300

    added a type to auth store

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -1,5 +1,11 @@
 import { reactive } from 'vue';
 
-export const auth = reactive({
-  authenticated: true
-})
+interface AuthStore {
+  authenticated: boolean;
+  token: string;
+}
+
+export const auth = reactive<AuthStore>({
+  authenticated: true,
+  token: '',
+});

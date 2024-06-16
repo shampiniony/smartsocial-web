dc4e8b296dc631735c30d4b18a860803c4364a40
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-06-16 03:46:08 +0300

    Good luck

--- src/layouts/admin.layout.vue
+++ src/layouts/admin.layout.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class='flex'>
+  <div v-if='auth.authenticated' class='flex'>
     <AdminHeader />
     <router-view />
+  </div>
+  <div v-else class='flex justify-center items-center h-screen w-screen'>
+    Please login
   </div>
 </template>
 
 <script setup>
 import AdminHeader from '@/components/admin-header.vue';
+import { auth } from '@/store/auth.store';
 </script>

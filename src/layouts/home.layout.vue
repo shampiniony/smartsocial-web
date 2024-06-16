7df1fe812fc18d19acb142f60c580ccdc26c6b5c
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-06-16 12:36:20 +0300

    fixed cart

--- src/layouts/home.layout.vue
+++ src/layouts/home.layout.vue
@@ -1,8 +1,10 @@
 <template>
   <Header />
   <router-view />
+  <Sheet />
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/header.vue'
+import Sheet from '@/models/cart/cart-sheet.vue'
 </script>
